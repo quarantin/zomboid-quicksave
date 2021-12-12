c9f0bf6c97196333502c95ce1fab8e0fa7a768a4
Corentin Delorme <codelorme@gmail.com>
2021-12-12 06:43:11 +0100

    Refactor class QuickSave: Use javamods Filesystem rather than ZomboidFilesystem to avoid breakage if it's changed

--- src/zombie/quicksave/QuickSave.java
+++ src/zombie/quicksave/QuickSave.java
@@ -11,9 +11,9 @@ import java.io.IOException;
 
 import se.krka.kahlua.integration.annotations.LuaMethod;
 
-import zombie.debug.DebugLog;
-import zombie.ZomboidFileSystem;
-import zombie.javamods.JavaMod;
+import zombie.javamods.Filesystem;
+import zombie.javamods.Log;
+import zombie.javamods.mod.JavaMod;
 
 
 public class QuickSave extends JavaMod {
@@ -21,18 +21,17 @@ public class QuickSave extends JavaMod {
 	public final static String modId = "QuickSave";
 
 	@Override
-	public List<Class<?>> getExposedClasses() {
+	public List<Class<?>> getClassesToExpose() {
 		return null;
 	}
 
 	@Override
-	public List<Object> getGlobalObjects() {
+	public List<Object> getObjectsWithGlobalFunctions() {
 		return Arrays.asList(this);
 	}
 
 	@Override
 	public void startup() {
-		DebugLog.Lua.println("QUICKSAVE STARTUP!");
 	}
 
 	@LuaMethod(name = "QuickSave", global = true)
@@ -43,21 +42,6 @@ public class QuickSave extends JavaMod {
 	@LuaMethod(name = "QuickLoad", global = true)
 	public void QuickLoad(String saveDir) {
 		QuickSaveLoad(saveDir, false);
-	}
-
-	private static String getSavePath() {
-		return ZomboidFileSystem.instance.getSaveDir();
-	}
-
-	private static String getBackupPath() {
-
-		String backupPath = ZomboidFileSystem.instance.getCacheDirSub("QuickSaves");
-		File backupDir = new File(backupPath);
-
-		if (!backupDir.exists())
-			backupDir.mkdirs();
-
-		return backupPath;
 	}
 
 	private static void deleteRecursively(File root) {
@@ -90,7 +74,7 @@ public class QuickSave extends JavaMod {
 			output.close();
 		}
 		catch (IOException error) {
-			DebugLog.Lua.warn(modId + ": I/O error occured while copying file: " + inputFile);
+			Log.warn(modId + ": I/O error occured while copying file: " + inputFile);
 		}
 	}
 
@@ -109,40 +93,38 @@ public class QuickSave extends JavaMod {
 		}
 	}
 
-	public static void QuickSaveLoad(String saveDir, boolean save) {
+	public static void QuickSaveLoad(String savePath, boolean save) {
 
-		String inputPath;
-		String outputPath;
+		File inputDir, outputDir;
+		Log.info(modId + ": Saving game " + savePath);
 
-		DebugLog.Lua.println("QUICKSAVE: SAVING GAME " + saveDir);
-
-		if (saveDir == null || saveDir.equals("")) {
-			DebugLog.Lua.warn(modId + ": saveDir is missing");
+		if (savePath == null || savePath.equals("")) {
+			Log.warn(modId + ": savePath is missing");
 			return;
 		}
 
-		if (saveDir.contains("..")) {
-			DebugLog.Lua.warn(modId + ": relative paths not allowed");
+		if (savePath.contains("..")) {
+			Log.warn(modId + ": relative paths not allowed");
 			return;
 		}
 
-		saveDir = saveDir.replace("/", File.separator);
-		saveDir = saveDir.replace("\\", File.separator);
+		savePath = savePath.replace("/",  File.separator);
+		savePath = savePath.replace("\\", File.separator);
+
+		File saveDir   = Filesystem.getUserProfileSaveDir();
+		File backupDir = Filesystem.getUserProfileSubdir(modId + "s" );
 
 		if (save) {
-			inputPath = getSavePath();
-			outputPath = getBackupPath();
+			inputDir  = new File(saveDir,   savePath);
+			outputDir = new File(backupDir, savePath);
 		}
 		else {
-			inputPath = getBackupPath();
-			outputPath = getSavePath();
+			inputDir  = new File(backupDir, savePath);
+			outputDir = new File(saveDir,   savePath);
 		}
 
-		File inputDir = new File(inputPath, saveDir);
-		File outputDir = new File(outputPath, saveDir);
-
 		if (!inputDir.exists()) {
-			DebugLog.Lua.warn(modId + ": Path does not exists: " + inputDir.getAbsolutePath());
+			Log.warn(modId + ": Path does not exists: " + inputDir.getAbsolutePath());
 			return;
 		}
 
